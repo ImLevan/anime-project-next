ba36d88b36ae92224aa77976ed3c2dcf5dac39e7
ImLevan <chianesevalentin@gmail.com>
2024-08-30 12:31:04 -0300

    nodemailer config v3

--- src/libs/nodeMailer.js
+++ src/libs/nodeMailer.js
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
     //Usar .env para los datos sensibles
     host: 'smtp.gmail.com',
     port: 587,
-    secure: false,
+    secure: process.env.NODE_ENV === 'production',
     auth: {
         user: email,
         pass: pass
@@ -19,7 +19,7 @@ const transporter = nodemailer.createTransport({
 
 export async function sendMail(mail, code) {
     const info = await transporter.sendMail({
-        from: process.env.EMAIL_USER,
+        from: email,
         to: mail,
         subject: 'Codigo de confirmacion',
         text: `Su codigo de verificacion es: ${code}\nEste codigo expira en 1 hora.\n\nSi no fue usted, por favor ignore este correo.\n\nGracias por registrarse en AnimeManagment`,
