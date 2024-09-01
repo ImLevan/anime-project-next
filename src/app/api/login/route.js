7d98204d203c94d10a53dff9c8c87d7420c7bbae
ImLevan <chianesevalentin@gmail.com>
2024-09-01 18:12:30 -0300

    fix redirect v8

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -130,7 +130,6 @@ export async function POST(request) {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: 'none',
-            domain: process.env.NEXT_PUBLIC_SITE_URL === 'http://localhost:3000' ? '' : process.env.NEXT_PUBLIC_SITE_URL,
             maxAge: 1000 * 60 * 60 * 24 * 30,
             path: '/'
         })
